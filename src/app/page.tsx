1675b6159c9c5f26fef1721f397253e11d12ee2e
s2hfigures-stack <s2h.figures@gmail.com>
2026-05-22 21:47:06 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import Navbar from '@/components/layout/Navbar'
 import Footer from '@/components/layout/Footer'
 import Hero from '@/components/sections/Hero'
@@ -18,6 +19,49 @@ export default function Home() {
         <Hero />
         <Marquee />
         <Products />
+        <div id="product-component-1779379310646"></div>
+        <Script
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function () {
+        var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+
+        function loadScript() {
+          var script = document.createElement('script');
+          script.async = true;
+          script.src = scriptURL;
+          document.head.appendChild(script);
+          script.onload = ShopifyBuyInit;
+        }
+
+        function ShopifyBuyInit() {
+          var client = ShopifyBuy.buildClient({
+            domain: 'buj9fc-vb.myshopify.com',
+            storefrontAccessToken: '45f6a6055a18b587ed88716ffa9b826e',
+          });
+
+          ShopifyBuy.UI.onReady(client).then(function (ui) {
+            ui.createComponent('product', {
+              id: '7544913723463',
+              node: document.getElementById('product-component-1779379310646'),
+            });
+          });
+        }
+
+        if (window.ShopifyBuy) {
+          if (window.ShopifyBuy.UI) {
+            ShopifyBuyInit();
+          } else {
+            loadScript();
+          }
+        } else {
+          loadScript();
+        }
+      })();
+    `,
+  }}
+/>
         <Marquee />
         <HowItWorks />
         <WhyUs />
